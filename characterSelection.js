f7ddb29edca2580da6909ae0507e5b8a3730b2b9
jaden <99984344+pajama10000@users.noreply.github.com>
2025-04-27 20:50:46 -0400

    e
    e

--- characterSelection.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('man').onclick = function() {
-    localStorage.setItem('character', 'man');
-    window.location.href = 'game.html';
-}
-
-document.getElementById('zombie').onclick = function() {
-    localStorage.setItem('character', 'zombie');
-    window.location.href = 'game.html';
-}
